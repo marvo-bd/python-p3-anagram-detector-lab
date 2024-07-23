778dcc85cf1933b60dc212d8dffb95a72faf6a6d
Marvin Kabira <marvobd@gmail.com>
2024-07-23 13:16:27 +0300

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+#!/usr/bin/env python3
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        def normalize(word):
+            return ''.join(sorted(word.lower()))
+        
+        normalized_word = normalize(self.word)
+        return [word for word in possible_anagrams if normalize(word) == normalized_word]
